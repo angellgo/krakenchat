7477961357b5ae9c0d79897f248cd69066ac8763
kike <go334506@uaeh.edu.mx>
2019-05-12 00:56:59 -0500

    01InicioDeFrontend

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,14 @@
+  
+<nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+        <a id="logo-container" href="#" class="brand-logo">Logo</a>
+        <ul class="right hide-on-med-and-down">
+        <li><a href="{{route('user.register')}}">Registrarse</a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">Iniciar Sesión</a></li>
+        </ul>
+        <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+</nav>
---
+++ resources/views/layouts/tools.blade.php
@@ -0,0 +1,12 @@
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+   
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
+    <!--  Scripts-->
+    <script src="{{ asset('js/materialize.js') }}"></script>
+    <script src="{{ asset('js/init.js') }}"></script>
