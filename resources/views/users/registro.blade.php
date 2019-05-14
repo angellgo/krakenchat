bd328f6a0be1d6478a3067158ea02741d351ef3f
wanheda <angel.de.oz.97@gmail.com>
2019-05-14 10:21:44 -0500

    cambioporpendejox2

--- resources/views/users/registro.blade.php
+++ resources/views/users/registro.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login - KrakenChat</title>
+    <title>Registro - KrakenChat</title>
     @include('layouts.tools')
     <script>      
             document.addEventListener('DOMContentLoaded', function() {
